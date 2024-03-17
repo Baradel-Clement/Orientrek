a43e21cdfcba53b6bba2fc8c2461236bdd651855
Baradel-Clement <baradelclement@gmail.com>
2024-03-17 16:46:22 +0100

    cookie done

--- components/CookieBanner.tsx
+++ components/CookieBanner.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { getLocalStorage, setLocalStorage } from "../lib/storageHelper";
+import { useRouter } from "next/router";
 
 export default function CookieBanner() {
+  const router = useRouter();
+
   const [cookieConsent, setCookieConsent] = useState(false);
+  const [bannerDisplay, setBannerDisplay] = useState(true);
 
   useEffect(() => {
     const storedCookieConsent = getLocalStorage("cookie_consent", null);
@@ -19,28 +23,37 @@ export default function CookieBanner() {
     });
 
     setLocalStorage("cookie_consent", cookieConsent);
-
-    //For Testing
-    console.log("Cookie Consent: ", cookieConsent);
   }, [cookieConsent]);
-  return (
-    <div className={`CookieBanner ${cookieConsent != null ? "none" : ""} `}>
-      <div className="">
-        <Link href="/mentions-legales">
-          <p>
-            Nous utilisons des <span className="">cookies</span> sur notre site.
-          </p>
-        </Link>
-      </div>
 
-      <div className="">
-        <button className="" onClick={() => setCookieConsent(false)}>
-          Refuser
-        </button>
-        <button className="" onClick={() => setCookieConsent(true)}>
-          Accepter
-        </button>
-      </div>
-    </div>
-  );
+  if (router.pathname !== "/mentions-legales") {
+    return (
+      <>
+        <div
+          onClick={() => setBannerDisplay(false)}
+          className={`CookieBanner-shadow ${
+            cookieConsent != null || bannerDisplay === false ? "none" : ""
+          } `}
+        />
+        <div
+          className={`CookieBanner ${
+            cookieConsent != null || bannerDisplay === false ? "none" : ""
+          } `}
+        >
+          <Link href="/mentions-legales">
+            Nous utilisons des <span className="aqua">cookies</span> sur notre
+            site.
+          </Link>
+
+          <div className="">
+            <button className="" onClick={() => setCookieConsent(false)}>
+              Refuser
+            </button>
+            <button className="" onClick={() => setCookieConsent(true)}>
+              Accepter
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
