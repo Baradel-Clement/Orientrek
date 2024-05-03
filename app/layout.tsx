a359939f3afcdd89fc31c15930c3534b58f6b815
Clément Baradel <baradelclement@gmail.com>
2024-05-03 18:46:54 +0200

    files to app

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@
 import { AppProps } from "next/app";
 import "../styles/index.scss";
 import { Metadata, NextPage } from "next";
-import { ReactElement, ReactNode } from "react";
+import { ReactElement, ReactNode, Suspense } from "react";
 import { HomeContextProvider } from "../context/Home";
 import GoogleAnalytics from "../components/GoogleAnalytics";
 
@@ -15,7 +15,9 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body>
-        <GoogleAnalytics GA_MEASUREMENT_ID="G-88JTZML9Q3" />
+        <Suspense fallback>
+          <GoogleAnalytics GA_MEASUREMENT_ID="G-88JTZML9Q3" />
+        </Suspense>
         <HomeContextProvider>{children}</HomeContextProvider>
       </body>
     </html>
