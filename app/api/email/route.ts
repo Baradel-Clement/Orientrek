e8181b90f67970eadebdb2918d504f5cc9fa5d7a
Clément Baradel <baradelclement@gmail.com>
2024-05-03 13:20:20 +0200

    files to app

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { resend } from "../../../lib/resend";
 
-export default async function POST(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
